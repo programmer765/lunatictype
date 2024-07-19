930f64c673c8e4d26db0ad9461d609468d75a727
Aviprit Shah <avipritshah15@gmail.com>
2024-07-20 01:50:40 +0530

    updated navbar color

--- apps/frontend/src/Components/Navbar.tsx
+++ apps/frontend/src/Components/Navbar.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
     return (
         <div className="py-5 text-white px-2">
-            <div className="text-center text-5xl font-semibold font-mono text-{#E5F4E3}">
+            <div className="text-center text-5xl font-semibold font-mono text-[#3584C5]">
                 Lunatictype
             </div>
         </div>
