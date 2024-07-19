df4631bfeac28fd2d90ae3e9eeb02a5712472d55
Aviprit Shah <avipritshah15@gmail.com>
2024-07-19 23:59:40 +0530

    added navbar

---
+++ apps/frontend/src/Components/Navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+    return (
+        <div className="py-5 text-white px-2">
+            <div className="text-center text-5xl font-semibold font-mono text-{#E5F4E3}">
+                Lunatictype
+            </div>
+        </div>
+    );
+};
--- apps/frontend/src/Pages/LandingPage.tsx
+++ apps/frontend/src/Pages/LandingPage.tsx
@@ -1,8 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import React from 'react'
+import {Navbar} from "../Components/Navbar.tsx";
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className='h-screen bg-[#202020]'>
+        <div className=''>
+            <Navbar />
+        </div>
+    </div>
   )
 }
 
