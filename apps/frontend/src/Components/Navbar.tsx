5558fb4508956f89a7d95e5aafdf9947e90bedd0
Aviprit Shah <avipritshah15@gmail.com>
2024-07-21 23:58:24 +0530

    added link for home tab

--- apps/frontend/src/Components/Navbar.tsx
+++ apps/frontend/src/Components/Navbar.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
     return (
         <div className="py-5 text-white px-2">
-            <div className="text-center text-5xl font-semibold font-mono text-[#3584C5]">
-                Lunatictype
+            <div className="text-center text-5xl font-semibold font-mono text-[#d4dce4] hover:cursor-pointer">
+                <Link to="/">
+                    Lunatictype
+                </Link>
             </div>
         </div>
     );
