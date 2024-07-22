c9912aae71f0ab3d21a3fad627a225a356d89ee3
Aviprit Shah <avipritshah15@gmail.com>
2024-07-22 23:49:09 +0530

    added login and singup buttons

--- apps/frontend/src/Components/Navbar.tsx
+++ apps/frontend/src/Components/Navbar.tsx
@@ -1,12 +1,20 @@
+import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
     return (
-        <div className="py-5 text-white px-2">
-            <div className="text-center text-5xl font-semibold font-mono text-[#d4dce4] hover:cursor-pointer">
-                <Link to="/">
-                    Lunatictype
-                </Link>
+        <div className="py-5 text-white px-4">
+            <div className="flex justify-between">
+                <div className="text-5xl font-semibold font-mono text-[#d4dce4] hover:cursor-pointer">
+                    <Link to="/">
+                        Lunatictype
+                    </Link>
+                </div>
+                <div className="flex">
+                    <Button variant="contained">Log in</Button>
+                    {/* <div className="px-2">Log in</div> */}
+                    <div className="px-2">Sign up</div>
+                </div>
             </div>
         </div>
     );
