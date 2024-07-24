7a92c7c4fe6a790f854feeaaea8ce2938b26b379
Aviprit Shah <avipritshah15@gmail.com>
2024-07-24 23:59:47 +0530

    added shadow effect in login button

--- apps/frontend/src/Components/LoginButton.tsx
+++ apps/frontend/src/Components/LoginButton.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const LoginButton = () => {
   return (
-    <div className='items-center appearance-none text-lg bg-[#fff] rounded-[24px] border-none [box-shadow:rgba(0,_0,_0,_.2)_0_3px_5px_-1px,rgba(0,_0,_0,_.14)_0_6px_10px_0,rgba(0,_0,_0,_.12)_0_1px_18px_0] box-border text-[#3c4043] cursor-pointer inline-flex fill-current font-["Google_Sans",Roboto,Arial,sans-serif] text-[14px] font-medium h-[48px] justify-center tracking-[.25px] leading-[normal] max-w-full overflow-visible px-[24px] py-[2px] relative text-center normal-case [transition:box-shadow_280ms_cubic-bezier(.4,_0,_.2,_1),opacity_15ms_linear_30ms,transform_270ms_cubic-bezier(0,_0,_.2,_1)_0ms] select-none w-auto z-0 hover:bg-[#F6F9FE] hover:[box-shadow:rgba(100,_100,_111,_0.2)_0px_7px_29px_0px]'>
+    <div className='items-center appearance-none text-lg bg-[#fff] rounded-[24px] border-none [box-shadow:0px_2px_12px_rgb(255_255_255)] box-border text-[#3c4043] cursor-pointer inline-flex fill-current font-["Google_Sans",Roboto,Arial,sans-serif] text-[14px] font-medium h-[48px] justify-center tracking-[.25px] leading-[normal] max-w-full overflow-visible px-[24px] py-[2px] relative text-center normal-case [transition:box-shadow_325ms_cubic-bezier(.4,_0,_.2,_1),opacity_15ms_linear_30ms,transform_350ms_cubic-bezier(0,_0,_.2,_1)_0ms] select-none w-auto z-0 hover:bg-[#F6F9FE] hover:[box-shadow:rgba(100,_100,_111,_0.2)_0px_7px_29px_0px]'>
         Log in
     </div>
   )
