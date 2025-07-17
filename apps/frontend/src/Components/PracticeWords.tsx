60870b29a4e90ed2d2a7600501915b609dcf3e40
Aviprit Shah <avipritshah15@gmail.com>
2025-07-18 03:42:15 +0530

    keydown event added

--- apps/frontend/src/Components/PracticeWords.tsx
+++ apps/frontend/src/Components/PracticeWords.tsx
@@ -1,12 +1,29 @@
+import { useEffect } from 'react'
 import { useGetRandomWordFromServer } from '../server/router/getDataFromServer'
 
 const PracticeWords = () => {
 
   const words = useGetRandomWordFromServer()
 
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if(e.key.length === 1) {
+        console.log(e.key)
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown)
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
+
   return (
     <div>
-      <div className='text-white flex flex-wrap'>
+      <div className='relative text-gray-500 flex flex-wrap text-justify'>
+        <div className='absolute w-[1px] h-10 bg-white animate-[cursor-blink] left-2'>
+        </div>
         {
           words.map((word, index) => (
           <span key={index} className='mx-2 text-3xl'>{word}</span>
