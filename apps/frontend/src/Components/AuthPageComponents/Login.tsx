03f81d6dd9582ab3bae582a2e0d7afe610d53a0d
Aviprit Shah <avipritshah15@gmail.com>
2025-08-19 23:57:28 +0530

    progressed with auth page

--- apps/frontend/src/Components/AuthPageComponents/Login.tsx
+++ apps/frontend/src/Components/AuthPageComponents/Login.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Login : React.FC = () => {
   return (
-    <div>Login</div>
+    <div>
+      <div className='text-sm'>Please log in to continue.</div>
+      <div className='flex mt-4 space-x-4'>
+        <div>
+          Google
+        </div>
+        <div>
+          Github
+        </div>
+        <div>
+          Apple
+        </div>
+      </div>
+    </div>
   )
 }
 
--- apps/frontend/src/Pages/AuthPage.tsx
+++ apps/frontend/src/Pages/AuthPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { useLocation } from 'react-router-dom'
+import Login from '../Components/AuthPageComponents/Login'
+import Signup from '../Components/AuthPageComponents/Signup'
 
 const AuthPage : React.FC = () => {
 
@@ -11,12 +13,18 @@ const AuthPage : React.FC = () => {
   return (
     <motion.div className="flex items-center justify-center h-screen bg-gradient-to-br from-[#141220] to-[#000000]">
       <div className='w-full px-32 py-12 h-screen flex items-center justify-center'>
-        <div className='w-1/2 flex flex-col bg-black h-full text-white rounded-l-lg shadow-lg'>
+        <div className='w-1/3 flex flex-col bg-black h-full text-white rounded-l-lg shadow-lg'>
           <div className='flex items-center justify-center py-10'>
-            <h1 className='text-4xl font-semibold'>LunaticType</h1>
+            <h1 className='text-3xl font-semibold'>LunaticType</h1>
           </div>
-          <div className='flex flex-col px-16 pt-20'>
-            <div className='text-2xl'>Welcome!</div>
+          <div className='flex flex-col px-20 pt-16'>
+            <div className='text-xl pb-2'>Welcome!</div>
+            {
+              (from === 'login') ? 
+                <Login />
+              :
+                <Signup />
+            }
           </div>
         </div>
         <div className='w-1/2 h-full bg-gradient-to-br from-[#100E1C] via-[#181626] to-[#000000] rounded-r-lg shadow-lg p-8'>
