aca65a39e93f78f26152f524352ad5c67ab36f97
Aviprit Shah <avipritshah15@gmail.com>
2025-09-14 22:38:46 +0530

    added rough cursor transition

--- apps/frontend/src/Components/PracticeWords.tsx
+++ apps/frontend/src/Components/PracticeWords.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { useGetRandomWordFromServer } from '../server/router/getDataFromServer'
 import Loading from './Loading'
 
@@ -10,8 +10,30 @@ const PracticeWords = () => {
   const [currentSentence, setCurrentSentence] = useState<string>('')
   const [wordSentence, setWordSentence] = useState<string>('')
   const [loading, setLoading] = useState<boolean>(true)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const chars = [...wordSentence]
+  const [cursorPosition, setCursorPosition] = useState<{x: number, y: number}>({x: 0, y: 0})
+
+  const textRef = useRef<HTMLDivElement>(null)
+  const characterRef = useRef<(HTMLSpanElement | null)[]>([])
+
+  const chars = useMemo(() => [...wordSentence], [wordSentence])
+
+  const updateCursorPosition = useCallback((index : number) => {
+    if(characterRef.current[index] === null || textRef.current === null) return
+    const char = characterRef.current[index]
+    const textContainer = textRef.current
+    
+    if(char) {
+      const charRect = char.getBoundingClientRect()
+      const containerRect = textContainer.getBoundingClientRect()
+
+      setCursorPosition({
+        x: charRect.left - containerRect.left,
+        y: charRect.top - containerRect.top + charRect.height / 8,
+      })
+    } else if(index === 0) {
+      setCursorPosition({x: 0, y: 0})
+    }
+  }, [])
   
   useEffect(() => {
     if(data.isLoading === true) {
@@ -27,8 +49,14 @@ const PracticeWords = () => {
     const sentence : string = words.join(' ')
 
     setWordSentence(sentence)
+    setCurrentSentence('')
+    updateCursorPosition(0)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data.isLoading])
+
+  useEffect(() => {
+    updateCursorPosition(currentSentence.length)
+  }, [currentSentence.length, updateCursorPosition, chars])
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
@@ -62,7 +90,11 @@ const PracticeWords = () => {
     <div>
       { loading && <Loading />}
       <div className='relative flex flex-wrap text-justify'>
-        <div className='mx-2 text-4xl tracking-wider'>
+        <div ref={textRef} className='mx-2 text-4xl tracking-wider'>
+        <div 
+          className='absolute w-[1px] h-10 bg-white transition-transform duration-300 ease-in-out'
+          style={{ transform: `translate(${cursorPosition.x}px, ${cursorPosition.y}px)`}}
+        />
           {
             chars.map((char, index) => {
 
@@ -80,19 +112,22 @@ const PracticeWords = () => {
                 else colorClass = (char === currentSentence[index]) ? 'text-white' : 'text-red-500' 
               }
               
-              const isCursor = index === currentSentence.length
+              // const isCursor = index === currentSentence.length
 
               return (
-                <span key={index}>
-                  <span key={index} className={`letter ${colorClass}`}>
-                    {
-                    isCursor && 
-                      <span 
-                          className='absolute w-[1px] h-10 bg-white animate-[cursor-blink] transition-transform duration-300 ease-in-out' />
-                    }
+                // <span key={index}>
+                  <span 
+                    key={index} 
+                    ref={(el) => {
+                      // if(index === 0) characterRef.current[index] = null
+                      characterRef.current[index] = el
+                      // console.log(characterRef.current[index])
+                    }}
+                    className={`letter ${colorClass} transform duration-600 ease-in-out`}
+                  >
                     {displayChar}
                   </span>
-                </span>
+                // </span>
               )
             })
           }
