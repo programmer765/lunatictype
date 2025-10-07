cfab62eec427c5581e8032a9d2361f63c81f1f52
Aviprit Shah <avipritshah15@gmail.com>
2025-10-07 22:35:51 +0530

    added PORT env for starting server

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -44,6 +44,8 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use('/api', createExpressMiddleware({ router: appRouter, createContext }));
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
