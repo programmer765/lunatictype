362b70501069010a55d3e47ec2b45b2210dfebe7
Aviprit Shah <avipritshah15@gmail.com>
2026-02-14 23:59:03 +0530

    added table migrations

---
+++ apps/backend/prisma/migrations/20260213105807/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "username" TEXT NOT NULL,
+    "password" TEXT,
+    "picture" TEXT,
+    "google_id" TEXT,
+    "github_id" TEXT,
+    "is_google_verified" BOOLEAN NOT NULL DEFAULT false,
+    "is_github_verified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_google_id_key" ON "User"("google_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_github_id_key" ON "User"("github_id");
