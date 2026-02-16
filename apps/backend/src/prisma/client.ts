c876736722c42f13f3d3945a8820e75d400444e5
Aviprit Shah <avipritshah15@gmail.com>
2026-02-16 20:00:06 +0530

    fixed database url being string issue

--- apps/backend/src/prisma/client.ts
+++ apps/backend/src/prisma/client.ts
@@ -1,13 +1,9 @@
-// import dotenv from 'dotenv';
-// // dotenv.config();
-// const envFile = process.env.NODE_ENV === 'development' ? '.env.local' : '.env.production';
-// dotenv.config({ path: envFile });
 import { database_url } from "../env";
 import { PrismaClient } from "./generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg(database_url)
-console.log(database_url)
+const adapter = new PrismaPg({ database_url })
+
 const client = new PrismaClient({ adapter });
 
 export default client;
