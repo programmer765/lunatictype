df4da46b783dbda5d42dcd7fd34980423463b734
Aviprit Shah <avipritshah15@gmail.com>
2026-02-17 23:53:19 +0530

    store database url inside a connection string

--- apps/backend/src/prisma/client.ts
+++ apps/backend/src/prisma/client.ts
@@ -2,7 +2,9 @@ import { database_url } from "../env";
 import { PrismaClient } from "./generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ database_url })
+const connectionString = `${database_url}`;
+
+const adapter = new PrismaPg({ connectionString })
 
 const client = new PrismaClient({ adapter });
 
