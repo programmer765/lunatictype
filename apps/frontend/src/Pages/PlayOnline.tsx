a409d585568d0706356a8a33e964ede6c38a0be7
Aviprit Shah <avipritshah15@gmail.com>
2026-02-21 23:45:09 +0530

    added play online page

--- apps/frontend/src/Pages/PlayOnline.tsx
+++ apps/frontend/src/Pages/PlayOnline.tsx
@@ -1,12 +1,30 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
 const PlayOnline : React.FC = () => {
   return (
-    <div className="h-full flex items-center justify-center">
-      <div className='text-3xl font-mono text-white'>
-        Coming Soon...
+    <motion.div className="h-full flex items-center justify-center" 
+      initial={{ opacity: 0 }} 
+      animate={{ opacity: 1 }} 
+      transition={{ duration: 0.5 }}
+    >
+      <div className='text-3xl font-mono text-white flex gap-10'>
+        <motion.button className='px-10 py-5 bg-rose-800 hover:bg-rose-900 rounded-[27px]'
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          transition={{ duration: 0.3, ease: "easeIn" }}
+        >
+          Create Room
+        </motion.button>
+        <motion.button className='px-10 py-5 bg-green-700 hover:bg-green-800 rounded-[27px]'
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          transition={{ duration: 0.3, ease: "easeIn" }}
+        >
+          Play 1v1
+        </motion.button>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
